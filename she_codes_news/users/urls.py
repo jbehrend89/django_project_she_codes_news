e46c563d0c8ac34b649f3b4a745eafc3feb2d7ab
jbehrend89 <84775195+jbehrend89@users.noreply.github.com>
2021-07-29 18:14:34 +0800

    Updated nav with user link

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import CreateAccountView
+from .views import UserProfileView
 
 app_name = 'users'
 
 urlpatterns = [
     path('create-account/', CreateAccountView.as_view(),name='createAccount'),
+    path('profile/<int:pk>/', UserProfileView.as_view(),name='profile'),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -3,10 +3,12 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from django.views.generic.detail import DetailView
 
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
 
-    
+class UserProfileView(DetailView):
+    model = CustomUser    
